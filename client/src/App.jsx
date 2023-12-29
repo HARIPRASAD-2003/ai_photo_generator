1cf1a4766beda1163016fccfdb15285b14ca50b0
HARIPRASAD <hari11122003@gmail.com>
2023-12-29 10:00:41 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
+import { logo } from './assets';
+import { Home, CreatePost } from './page';
+
+const App = () => (
+  <BrowserRouter>
+    <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+      <Link to="/">
+        <img src={logo} alt="logo" className="w-28 object-contain" />
+      </Link>
+      <div className='d-flex justify-content right '>
+      <Link to="/" className='mr-5 text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>Community</Link>
+      <Link to="/create-post" className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Create</Link>
+      </div>
+    </header>
+    <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-post" element={<CreatePost />} />
+      </Routes>
+    </main>
+  </BrowserRouter>
+);
+
+export default App;
